5540a32ef3f678fef7faa41303f294f125ddb7df
MissUnderstoodKnight <sebabaddon@gmail.com>
2020-09-15 16:34:17 -0300

    searchform & css changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import 'bulma/css/bulma.css'
+import 'bulma/css/bulma.css';
 
-import { Title } from './components/Title'
+import { Title } from './components/Title';
+import { SearchForm } from './components/searchForm';
 
 function App() {
   return (
     <div className="App">
       <Title>Movies</Title>
+      <div className="SearchForm-wrapper">
+        <SearchForm />
+      </div>
     </div>
   );
 }
