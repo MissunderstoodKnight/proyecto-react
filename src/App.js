5c060ec13af45478fdf4a3a3152ff1f8f2bc17d8
MissUnderstoodKnight <sebabaddon@gmail.com>
2020-09-18 02:50:08 -0300

    Separate home from app and church and skate

--- src/App.js
+++ src/App.js
@@ -2,44 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 
-import { Title } from './components/Title';
-import { SearchForm } from './components/SearchForm';
-import { MoviesList } from './components/MoviesList';
-
+import { Home } from './pages/Home'
 import { Detail } from './pages/Detail';
 
 class App extends Component {
 
-  state = { results: [], usedSearch: false }
-  
-  _handleResults = (results) => {
-    this.setState({results, usedSearch: true})
-  }
-
-  _renderResults () {
-    return this.state.results.length === 0 
-      ? <p>Sin resultados</p>
-      : <MoviesList movies={this.state.results}/>
-  }
-  
   render() {
     const url = new URL(document.location)
-    const hasId = url.searchParams.has('id')
-
-    if (hasId) {
-      return <Detail id={url.searchParams.get('id')}/>
-    }
+    const Page = url.searchParams.has('id')
+      ? <Detail id={url.searchParams.get('id')}/>
+      :<Home />
     
     return (
       <div className="App">
-        <Title>Movies</Title>
-        <div className="SearchForm-wrapper">
-          <SearchForm onResults={this._handleResults}/>
-        </div>
-        {this.state.usedSearch 
-          ? this._renderResults()
-          : <small>Ingresa el nombre de la pelicula que deseas buscar</small>}
-      </div>
+        {Page}
+      </div>  
     );
   }
 }
