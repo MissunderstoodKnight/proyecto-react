7a83f710c9fb3743e0b45f710d0ef3c3446ebde9
MissUnderstoodKnight <sebabaddon@gmail.com>
2020-09-21 01:08:21 -0300

    pagination with react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
+import { Switch, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail';
@@ -8,14 +9,12 @@ import { Detail } from './pages/Detail';
 class App extends Component {
 
   render() {
-    const url = new URL(document.location)
-    const Page = url.searchParams.has('id')
-      ? <Detail id={url.searchParams.get('id')}/>
-      :<Home />
-    
     return (
       <div className="App">
-        {Page}
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/detail/:id' component={Detail} />
+        </Switch>
       </div>  
     );
   }
